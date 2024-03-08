9b788b29a3baf877e6fff61e1ef1e0b5b0f2ceae
shubham gurjar <shubhamgurjar222@gmail.com>
2024-03-08 14:21:52 +0530

    Word Replace Programe

---
+++ Word Replacement/word_replace.py
@@ -0,0 +1,12 @@
+def word_replace():
+    str = "Hi guys this is test String which will change"
+    print(str)
+    word_to_replace = input("enter the word to replace ")
+    word_replacement = input("enter the  word replacement ")
+    print(str.replace(word_to_replace, word_replacement))
+
+
+
+word = word_replace()
+print(word)
+    
